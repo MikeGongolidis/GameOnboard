45837a02cecaccf77de5731a21f276abaf8a1b61
juohmaru94 <dimmoe@hotmail.com>
2022-10-09 14:09:36 +0300

    -created menu box with 4 buttons which have JS

--- frontend/tic_tac_toe.js
+++ frontend/tic_tac_toe.js
@@ -1,4 +1,15 @@
-document.querySelector(".start-game").addEventListener("click", function () {
-  let elems = document.querySelectorAll(".cell-inner");
-  for (let i = 0; i < elems.length; i++) elems[i].textContent = "";
-});
+document.getElementById("back").onclick = function () {
+  location.href = "http://localhost:5500/frontend/index.html";
+};
+
+document.getElementById("start").onclick = function () {
+  document.querySelector(".menu").style.opacity = 0;
+};
+
+document.getElementById("inv").onclick = function () {
+  console.log("Player invite initiated");
+};
+
+document.getElementById("find").onclick = function () {
+  console.log("Finding game for you. Please wait :)");
+};
