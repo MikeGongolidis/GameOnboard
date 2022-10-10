9f8d9bd9d7a6671e0c9d54a8672232bb81bc987b
juohmaru94 <dimmoe@hotmail.com>
2022-10-10 18:19:36 +0300

    minor changes

--- frontend/tic_tac_toe.js
+++ frontend/tic_tac_toe.js
@@ -1,15 +1,26 @@
-document.getElementById("back").onclick = function () {
+document.getElementById("back").addEventListener("click", function () {
   location.href = "http://localhost:5500/frontend/index.html";
-};
+});
 
-document.getElementById("start").onclick = function () {
+document.getElementById("start").addEventListener("click", function () {
   document.querySelector(".menu").style.opacity = 0;
-};
+  document.querySelector(".menu").style.visibility = "hidden";
+});
 
-document.getElementById("inv").onclick = function () {
+document.getElementById("inv").addEventListener("click", function () {
   console.log("Player invite initiated");
-};
+});
 
-document.getElementById("find").onclick = function () {
+document.getElementById("find").addEventListener("click", function () {
   console.log("Finding game for you. Please wait :)");
-};
+});
+
+var myElementToCheckIfClicksAreInsideOf = document.getElementById("1");
+// Listen for click events on body
+document.body.addEventListener("click", function (event) {
+  if (myElementToCheckIfClicksAreInsideOf.contains(event.target)) {
+    console.log("clicked inside");
+  } else {
+    console.log("clicked outside");
+  }
+});
