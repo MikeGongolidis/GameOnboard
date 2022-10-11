4db15cd4c9381c04fb8fec1614af27d25ec35b1b
juohmaru94 <dimmoe@hotmail.com>
2022-10-11 20:58:22 +0300

    bug fix on animation

--- frontend/tic_tac_toe.js
+++ frontend/tic_tac_toe.js
@@ -17,12 +17,36 @@ document.getElementById("find").addEventListener("click", function () {
   console.log("Finding game for you. Please wait :)");
 });
 
-var myElementToCheckIfClicksAreInsideOf = document.getElementById("1");
+// Testing clicks on empty square
+let topLeft = document.getElementById("one");
+let topMiddle = document.getElementById("two");
+let topRight = document.getElementById("three");
+let middleLeft = document.getElementById("four");
+let middle = document.getElementById("five");
+let middleRight = document.getElementById("six");
+let bottomLeft = document.getElementById("seven");
+let bottomMiddle = document.getElementById("eight");
+let bottomRight = document.getElementById("nine");
 // Listen for click events on body
 document.body.addEventListener("click", function (event) {
-  if (myElementToCheckIfClicksAreInsideOf.contains(event.target)) {
-    console.log("clicked inside");
-  } else {
-    console.log("clicked outside");
+  if (topLeft.contains(event.target)) {
+    document.getElementById("cell-one").innerHTML = "X";
+    // console.log("clicked inside");
+  } else if (topMiddle.contains(event.target)) {
+    document.getElementById("cell-two").innerHTML = "X";
+  } else if (topRight.contains(event.target)) {
+    document.getElementById("cell-three").innerHTML = "X";
+  } else if (middleLeft.contains(event.target)) {
+    document.getElementById("cell-four").innerHTML = "X";
+  } else if (middle.contains(event.target)) {
+    document.getElementById("cell-five").innerHTML = "X";
+  } else if (middleRight.contains(event.target)) {
+    document.getElementById("cell-six").innerHTML = "X";
+  } else if (bottomLeft.contains(event.target)) {
+    document.getElementById("cell-seven").innerHTML = "X";
+  } else if (bottomMiddle.contains(event.target)) {
+    document.getElementById("cell-eight").innerHTML = "X";
+  } else if (bottomRight.contains(event.target)) {
+    document.getElementById("cell-nine").innerHTML = "X";
   }
 });
