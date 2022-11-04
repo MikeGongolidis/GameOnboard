ff2fafaf2900024c45d5dd39a0e22804ebfdaba1
mikegongolidis <mikegongolidis@hotmail.gr>
2022-11-05 00:24:16 +0200

    a

--- codedeploy/start-server.sh
+++ codedeploy/start-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 
 
 /usr/local/bin/docker-compose -f docker-compose.yml up --build -d > /dev/null 2> /dev/null < /dev/null &
--- codedeploy/stop-server.sh
+++ codedeploy/stop-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 
 
 /usr/local/bin/docker-compose stop
