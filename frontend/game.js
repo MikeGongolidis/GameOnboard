461a7db48c8f55fb527b0fed909909c4c9c3e9b6
mikegongolidis <mikegongolidis@hotmail.gr>
2022-11-05 16:54:47 +0200

    fix redirect buttons

--- frontend/game.js
+++ frontend/game.js
@@ -34,13 +34,13 @@ socket.onopen = function(event){
     document.querySelector('.game-title').innerHTML = (GAME_TYPE == 1 ) ? 'Tic-Tac-Toe' : 'Connect 4';
   }else {
     // Otherwise go back to select which game you want to play
-    window.location.replace("http://http://gameonboard-frontend.s3-website.eu-central-1.amazonaws.com/frontend/index.html");
+    window.location.replace("http://http://gameonboard-frontend.s3-website.eu-central-1.amazonaws.com/index.html");
   }
 };
 
 
 document.getElementById("back").addEventListener("click", function () {
-  location.href = "http://http://gameonboard-frontend.s3-website.eu-central-1.amazonaws.com/frontend/index.html";
+  location.href = "http://http://gameonboard-frontend.s3-website.eu-central-1.amazonaws.com/index.html";
 });
 
 // INVITE A FRIEND
